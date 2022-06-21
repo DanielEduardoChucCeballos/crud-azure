e5ad3fd11a5e27482c45bea867ef2efc5fdd6d27
Daniel Chuc <chucdaniel115@gmail.com>
2022-06-21 09:27:52 -0500

    Cambios

--- index.php
+++ index.php
@@ -1,84 +1,144 @@
+<?php include 'template/header.php' ?>
+
 <?php
-include 'conexion.php';
-$con = connect();
-$sql = 'SELECT * FROM Students';
-$query = mysqli_query($con, $sql);
+    include_once "model/conexion.php";
+    $sentencia = $bd -> query("select * from persona");
+    $persona = $sentencia->fetchAll(PDO::FETCH_OBJ);
+    //print_r($persona);
 ?>
-<!DOCTYPE html>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-<body>
-<section class=" py-4 " style="background-color: #eee;">
-  <div class="container">
-    <div class="row d-flex justify-content-center align-items-center">
-      <div class="col-lg-12 col-xl-11">
-          
-        <div class="card text-black" style="border-radius: 25px;">
-          <div class="card-body p-md-5">
-            <center>
-              <h1 >Estudiantes </h1>
-            </center>
-              
-            <div class="row justify-content-center">
-                
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-    <a href="create.php" class="btn btn-primary text-end">Crear</a>
-
-              <table class="table">
-                  
-  <thead>
-      
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Apellido</th>
-      <th scope="col">Email</th>
-      <th scope="col">Teléfono</th>
-      <th scope="col">Acciones</th>
-
-
-    </tr>
-  </thead>
-  <tbody>
-        <?php while ($row = mysqli_fetch_array($query)) { ?>
-    <tr>
-      <th><?php echo $row['id']; ?></th>
-      <td><?php echo $row['fname']; ?></td>
-      <td><?php echo $row['lname']; ?></td>
-      <td><?php echo $row['email']; ?></td>
-      <td><?php echo $row['phone']; ?></td>
-      <td>
-        <a href="edit.php?id=<?php echo $row[
-            'id'
-        ]; ?>"  class="btn btn-info px-3">Editar</a>
-      <a href="delete.php?id=<?php echo $row[
-          'id'
-      ]; ?>"class="btn btn-danger">Eliminar</a>
-
-      </td>
-     
-    </tr>
-    <?php } ?>
-  </tbody>
-</table>
-
-
-              </div>
-
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <!-- inicio alerta -->
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'falta'){
+            ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Rellena todos los campos.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+            <?php 
+                }
+            ?>
 
-</body>
-</html>
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'registrado'){
+            ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Registrado!</strong> Se agregaron los datos.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?>   
+            
+            
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'error'){
+            ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Vuelve a intentar.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?>   
+
+
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'editado'){
+            ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Cambiado!</strong> Los datos fueron actualizados.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?> 
+
+
+            <?php 
+                if(isset($_GET['mensaje']) and $_GET['mensaje'] == 'eliminado'){
+            ?>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Eliminado!</strong> Los datos fueron borrados.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            <?php 
+                }
+            ?> 
+
+            <!-- fin alerta -->
+            <div class="card">
+                <div class="card-header">
+                    Lista de personas
+                </div>
+                <div class="p-4">
+                    <table class="table align-middle">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Edad</th>
+                                <th scope="col">Signo</th>
+                                <th scope="col" colspan="2">Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            <?php 
+                                foreach($persona as $dato){ 
+                            ?>
+
+                            <tr>
+                                <td scope="row"><?php echo $dato->codigo; ?></td>
+                                <td><?php echo $dato->nombre; ?></td>
+                                <td><?php echo $dato->edad; ?></td>
+                                <td><?php echo $dato->signo; ?></td>
+                                <td><a class="text-success" href="editar.php?codigo=<?php echo $dato->codigo; ?>"><i class="bi bi-pencil-square"></i></a></td>
+                                <td><a onclick="return confirm('Estas seguro de eliminar?');" class="text-danger" href="eliminar.php?codigo=<?php echo $dato->codigo; ?>"><i class="bi bi-trash"></i></a></td>
+                            </tr>
+
+                            <?php 
+                                }
+                            ?>
+
+                        </tbody>
+                    </table>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Ingresar datos:
+                </div>
+                <form class="p-4" method="POST" action="registrar.php">
+                    <div class="mb-3">
+                        <label class="form-label">Nombre: </label>
+                        <input type="text" class="form-control" name="txtNombre" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Edad: </label>
+                        <input type="number" class="form-control" name="txtEdad" autofocus required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Signo: </label>
+                        <input type="text" class="form-control" name="txtSigno" autofocus required>
+                    </div>
+                    <div class="d-grid">
+                        <input type="hidden" name="oculto" value="1">
+                        <input type="submit" class="btn btn-primary" value="Registrar">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'template/footer.php' ?>
